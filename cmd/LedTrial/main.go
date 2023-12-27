65b7fd59c432a43fa2bc8f3a5f4086118d049ca9
Garrison Stauffer <gar.stauffer@gmail.com>
2023-12-27 15:20:48 -0500

    add sample gpio integration for testing

--- cmd/LedTrial/main.go
+++ cmd/LedTrial/main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+import "github.com/stianeikeland/go-rpio/v4"
 
 func main() {
-    fmt.Println("Hello world")
-}
+	fmt.Println("Hello world")
+	err := rpio.Open()
+	defer func() {
+		err = rpio.Close()
+		if err != nil {
+			fmt.Println("error closing rpio: ", err)
+		}
+	}()
+	if err != nil {
+		panic(fmt.Errorf("error initializing rpio: %w", err))
+	}
 
+	foo := rpio.Pin(16)
+	foo.High()
+
+	for i := 0; i < 50; i++ {
+		time.Sleep(2000 * time.Millisecond)
+		foo.Toggle()
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module garrison-stauffer.com/garrisonpi-exploration
 
 go 1.21.5
+
+require github.com/stianeikeland/go-rpio/v4 v4.6.0 // indirect

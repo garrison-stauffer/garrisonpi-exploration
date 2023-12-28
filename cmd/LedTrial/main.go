6ca2277bef8bfc58f9215ad6d2d93a15ac0fb7fb
Garrison Stauffer <gar.stauffer@gmail.com>
2023-12-28 00:08:43 -0500

    use periph.io library for rgb

--- cmd/LedTrial/main.go
+++ cmd/LedTrial/main.go
@@ -2,28 +2,47 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"os"
+	"periph.io/x/conn/v3/physic"
+	"periph.io/x/conn/v3/spi"
+	"periph.io/x/conn/v3/spi/spireg"
+	"periph.io/x/devices/v3/nrzled"
 )
-import "github.com/stianeikeland/go-rpio/v4"
 
 func main() {
-	fmt.Println("Hello world")
-	err := rpio.Open()
-	defer func() {
-		err = rpio.Close()
-		if err != nil {
-			fmt.Println("error closing rpio: ", err)
-		}
-	}()
+	//spi.
+	var pin spi.PortCloser
+	pin, err := spireg.Open("12")
 	if err != nil {
-		panic(fmt.Errorf("error initializing rpio: %w", err))
+		fmt.Printf("error opening spi port 12: %v\n", err)
+		os.Exit(1)
+		return
+	}
+	defer func(pin spi.PortCloser) {
+		err := pin.Close()
+		if err != nil {
+			fmt.Printf("error closing spi port: %v\n", err)
+			os.Exit(1)
+		}
+	}(pin)
+
+	device, err := nrzled.NewSPI(pin, &nrzled.Opts{
+		NumPixels: 15,
+		Channels:  4,
+		Freq:      1100 * physic.Hertz,
+	})
+	if err != nil {
+		fmt.Printf("error opening nrzled device: %v\n", err)
+		os.Exit(1)
 	}
 
-	foo := rpio.Pin(16)
-	foo.High()
+	defer device.Halt()
 
-	for i := 0; i < 50; i++ {
-		time.Sleep(2000 * time.Millisecond)
-		foo.Toggle()
+	for {
+		device.Write()
+		write, err := device.Write([]byte{255, 255, 255, 255})
+		if err != nil {
+			return
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module garrison-stauffer.com/garrisonpi-exploration
 
 go 1.21.5
 
-require github.com/stianeikeland/go-rpio/v4 v4.6.0 // indirect
+require (
+	periph.io/x/conn/v3 v3.7.0
+	periph.io/x/devices/v3 v3.7.1
+)

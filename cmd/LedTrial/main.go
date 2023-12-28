97064a4c77362d2ed1443558260cf10ec29dab9c
Garrison Stauffer <gar.stauffer@gmail.com>
2023-12-28 01:02:02 -0500

    graceful close + alternating color

--- cmd/LedTrial/main.go
+++ cmd/LedTrial/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
 	"periph.io/x/conn/v3/physic"
 	"periph.io/x/conn/v3/spi"
 	"periph.io/x/conn/v3/spi/spireg"
 	"periph.io/x/devices/v3/nrzled"
 	"periph.io/x/host/v3"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -46,10 +49,32 @@ func main() {
 
 	defer device.Halt()
 
+	exit := make(chan os.Signal, 1) // we need to reserve to buffer size 1, so the notifier are not blocked
+	signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
+	go func() {
+
+	}()
+
+	selectOptions := [][]byte{
+		[]byte{255, 255, 255, 255},
+		[]byte{0, 0, 255, 255},
+		[]byte{255, 0, 0, 255},
+		[]byte{0, 255, 0, 255},
+	}
+
+	var option = 0
+
 	for {
-		_, err := device.Write([]byte{255, 255, 255, 255})
-		if err != nil {
+		select {
+		case <-exit:
 			return
+		default:
+			option = (option + 1) % 4
+			_, err := device.Write(selectOptions[option])
+			if err != nil {
+				return
+			}
+			time.Sleep(time.Second)
 		}
 	}
 }

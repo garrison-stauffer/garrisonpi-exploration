e4aa196f777eebaf21d80f9bbdece48c0a6a6571
Garrison Stauffer <gar.stauffer@gmail.com>
2023-12-30 01:02:46 -0500

    try new library for leds

--- cmd/LedTrial/main.go
+++ cmd/LedTrial/main.go
@@ -2,81 +2,40 @@ package main
 
 import (
 	"fmt"
+	ws281x "github.com/mcuadros/go-rpi-ws281x"
+	"image/color"
 	"os"
 	"os/signal"
-	"periph.io/x/conn/v3/physic"
-	"periph.io/x/conn/v3/spi"
-	"periph.io/x/conn/v3/spi/spireg"
-	"periph.io/x/devices/v3/nrzled"
-	"periph.io/x/host/v3"
 	"syscall"
+	"time"
 )
 
 func main() {
-	//spi.
-	foo, err := host.Init()
-	fmt.Printf("registered host: %+v\n", foo)
-	if err != nil {
-		fmt.Printf("error initializing host: %v\n", err)
-		os.Exit(1)
-		return
-	}
-	pin, err := spireg.Open("")
-	fmt.Printf("pin loaded: %+v\n", pin)
-	if err != nil {
-		fmt.Printf("error opening spi port 12: %v\n", err)
-		os.Exit(1)
-		return
-	}
-	defer func(pin spi.PortCloser) {
-		err := pin.Close()
-		if err != nil {
-			fmt.Printf("error closing spi port: %v\n", err)
-			os.Exit(1)
-		}
-	}(pin)
-
-	device, err := nrzled.NewSPI(pin, &nrzled.Opts{
-		NumPixels: 15,
-		Channels:  4,
-		Freq:      2500 * physic.KiloHertz,
+	foo, _ := ws281x.NewCanvas(5, 1, &ws281x.HardwareConfig{
+		Pin:       18,
+		StripType: ws281x.StripRGBW,
 	})
-	if err != nil {
-		fmt.Printf("error opening nrzled device: %v\n", err)
-		os.Exit(1)
-	}
+	_ = foo.Initialize()
+	foo.Set(0, 0, color.RGBA{
+		R: 255,
+		A: 128,
+	})
 
-	defer device.Halt()
+	time.Sleep(5 * time.Second)
+	foo.Set(0, 0, color.RGBA{
+		B: 255,
+		A: 128,
+	})
 
 	exit := make(chan os.Signal, 1) // we need to reserve to buffer size 1, so the notifier are not blocked
 	signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
 
-	//selectOptions := [][]byte{
-	//	[]byte{255, 255, 255, 128},
-	//	[]byte{0, 0, 255, 128},
-	//	[]byte{255, 0, 0, 128},
-	//	[]byte{0, 255, 0, 128},
-	//}
-	//
-	//var option = 0
-
-	_, err = device.Write([]byte{255, 0, 0, 0})
-	//device.Halt()
-	//time.Sleep(3 * time.Second)
-	//_, err = device.Write([]byte{0, 255, 0})
-	//device.Halt()
-	//time.Sleep(3 * time.Second)
-	//_, err = device.Write([]byte{0, 0, 255})
-	//time.Sleep(3 * time.Second)
-
+	//selectOptions :=
 	fmt.Println("awaiting exit?")
+
 	<-exit
-	fmt.Println("Exiting now, writing 0 to everything?")
-	var output = make([]byte, 4*5)
-	for i := 0; i < 5; i++ {
-		output = append(output, 0, 0, 0, 0)
-	}
-	device.Write(output)
+	fmt.Println("Closing...?")
+	_ = foo.Close()
 
 	return
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module garrison-stauffer.com/garrisonpi-exploration
 
 go 1.21.5
 
-require (
-	periph.io/x/conn/v3 v3.7.0
-	periph.io/x/devices/v3 v3.7.1
-)
+require github.com/mcuadros/go-rpi-ws281x v0.0.0-20180122015054-0d7c616b0b23
 
-require periph.io/x/host/v3 v3.8.2
+require (
+	github.com/kr/pretty v0.2.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+)
